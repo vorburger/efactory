8cfdcd146be4cc773f342d26f0a2c9cc87e24e7c
Michael Vorburger <mike@vorburger.ch>
2013-06-21 12:25:09 +0200

    DynamicEmfTest (still failing) uses ResourceProvider

--- com.googlecode.efactory.tests/src/com/googlecode/efactory/dynamic/tests/DynamicEmfTest.java
+++ com.googlecode.efactory.tests/src/com/googlecode/efactory/dynamic/tests/DynamicEmfTest.java
@@ -1,35 +1,24 @@
 package com.googlecode.efactory.dynamic.tests;
 
-import java.io.InputStream;
-
 import javax.inject.Inject;
 
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
-import org.eclipse.xtext.junit4.util.ParseHelper;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.googlecode.efactory.EFactoryInjectorProvider;
-import com.googlecode.efactory.building.ModelBuilder;
-import com.googlecode.efactory.eFactory.Factory;
 import com.googlecode.efactory.tests.util.ResourceProvider;
-import com.googlecode.efactory.tests.util.TestConstants;
 import com.googlecode.efactory.util.Find;
 
 @RunWith(XtextRunner.class)
 @InjectWith(EFactoryInjectorProvider.class)
 public class DynamicEmfTest {
 
-	@Inject ParseHelper<Factory> parser;
-	@Inject ResourceSet resourceSet;
+	@Inject ResourceProvider provider;
 	
 	/**
 	 * Disabled for now, because it kept breaking and took too much time to
@@ -38,8 +27,9 @@ public class DynamicEmfTest {
 	 * and as this (*.ecore) integration isn't really a main use case now
 	 * anymore, with Xcore.
 	 */
-	@Ignore
 	@Test public void testBuilding() throws Exception {
+		EObject testModel = provider.loadModel("res/BuilderTests/DynamicEmf.efactory");
+/*		
 		URIConverter uriConverter = resourceSet.getURIConverter();
 		URI uri = new ResourceProvider(resourceSet, TestConstants.PLUGIN_ID).getUri("res/BuilderTests/DynamicEmf.efactory");
 		InputStream in = uriConverter.createInputStream(uri);
@@ -48,6 +38,7 @@ public class DynamicEmfTest {
 		
 		ModelBuilder modelBuilder = new ModelBuilder();
 		EObject testModel = modelBuilder.build(factory);
+*/		
 		DynamicEObjectImpl person = Find.child(DynamicEObjectImpl.class, "Person", testModel);
 		Assert.assertEquals("Entity", person.eClass().getName());
 	}
