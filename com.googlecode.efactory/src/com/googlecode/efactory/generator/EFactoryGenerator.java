64e9cf5d13b7b0c9aaf3596f78541504cf5c16bd
Michael Vorburger <mike@vorburger.ch>
2013-08-09 19:02:47 +0200

    deactivating Xtend (due to some version dependency issues with for ongoing downgrading exercise)

---
+++ com.googlecode.efactory/src/com/googlecode/efactory/generator/EFactoryGenerator.java
@@ -0,0 +1,12 @@
+package com.googlecode.efactory.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+public class EFactoryGenerator implements IGenerator {
+
+	public void doGenerate(Resource input, IFileSystemAccess fsa) {
+	}
+
+}
