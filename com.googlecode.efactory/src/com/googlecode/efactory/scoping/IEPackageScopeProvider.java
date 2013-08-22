2c4777ad90693cf77d222f703a1befc8aeae58f8
Yann Andenmatten <yann.natacha@gmail.com>
2013-08-22 15:39:06 +0200

    Updated copyright info

--- com.googlecode.efactory/src/com/googlecode/efactory/scoping/IEPackageScopeProvider.java
+++ com.googlecode.efactory/src/com/googlecode/efactory/scoping/IEPackageScopeProvider.java
@@ -1,19 +1,13 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2002-2006 Sebastian Benz and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2013 Yann Andenmatten.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: 
- *   Sebastian Benz - Initial API and implementation
- *
- * </copyright>
- *
- * 
- */
+ * Contributors:
+ *     Yann Andenmatten - initial API and implementation
+ ******************************************************************************/
 package com.googlecode.efactory.scoping;
 
 import org.eclipse.emf.ecore.EClass;
