ce198946f961d4d7fb2fb84c51e29fd8bb10e0a0
Michael Vorburger <mike@vorburger.ch>
2013-12-02 14:34:34 +0100

    added fixLineEndings() to FormatterTest
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- com.googlecode.efactory.tests/src/com/googlecode/efactory/formatting/tests/FormatterTest.java
+++ com.googlecode.efactory.tests/src/com/googlecode/efactory/formatting/tests/FormatterTest.java
@@ -64,7 +64,31 @@ public class FormatterTest {
         Assert.assertNotNull(parseResult);
 		ICompositeNode rootNode = parseResult.getRootNode();
 		String formattedText = formatter.format(rootNode, 0, text.length()).getFormattedText();
+		formattedText = fixLineEndings(formattedText);
 		Assert.assertEquals(text, formattedText);
 	}
 
+
+	/**
+	 * Not 100% sure when this is needed,
+	 * but https://code.google.com/a/eclipselabs.org/p/spray/source/browse/tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/SprayFormatterTest.java
+	 * is doing this, so might as well be safe rather than sorry.. ;-)
+	 */
+	private String fixLineEndings(String formatted) {
+		if (isWindowsEnding()) {
+			formatted = formatted.replace("\r\n", "\n");
+		}
+		formatted = formatted.replace("\r\b", "\n");
+		formatted = formatted + getEnding();
+		return formatted;
+	}
+
+	private String getEnding() {
+		return isWindowsEnding() ? "" : /* "\r" */"";
+	}
+
+	private boolean isWindowsEnding() {
+		String ls = System.getProperty("line.separator");
+		return "\r\n".equals(ls);
+	}
 }
