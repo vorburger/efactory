4829641afd5bb05e6a26773abe4cfc71ac553dd0
mumesh <mumesh@temenos.com>
2014-01-08 16:57:41 +0530

    Added copyright information

--- com.googlecode.efactory/src/com/googlecode/efactory/values/EfactoryValueConverterService.java
+++ com.googlecode.efactory/src/com/googlecode/efactory/values/EfactoryValueConverterService.java
@@ -1,19 +1,13 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2002-2006 Sebastian Benz and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2013 Michael Vorburger.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: 
- *   Sebastian Benz - Initial API and implementation
- *
- * </copyright>
- *
- * 
- */
+ * Contributors:
+ *     Umesh - initial API and implementation
+ ******************************************************************************/
 package com.googlecode.efactory.values;
 
 import java.util.regex.Pattern;
