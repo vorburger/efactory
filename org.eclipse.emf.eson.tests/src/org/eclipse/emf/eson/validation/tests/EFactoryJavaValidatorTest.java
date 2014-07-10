01c1369b1aff951e7a40f55def1e05577aace8d2
Michael Vorburger <mike@vorburge>
2014-07-10 18:51:20 +0200

    Simplified test (equivalent coverage)
    Signed-off-by: Michael Vorburger <mike@vorburge>

--- org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/validation/tests/EFactoryJavaValidatorTest.java
+++ org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/validation/tests/EFactoryJavaValidatorTest.java
@@ -10,15 +10,12 @@
  ******************************************************************************/
 package org.eclipse.emf.eson.validation.tests;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.InputStream;
-import java.util.List;
 
 import javax.inject.Inject;
 
-import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -27,11 +24,7 @@ import org.eclipse.emf.eson.validation.EFactoryJavaValidator;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.junit4.util.ParseHelper;
-import org.eclipse.xtext.junit4.validation.AssertableDiagnostics;
 import org.eclipse.xtext.junit4.validation.ValidatorTester;
-import org.eclipse.xtext.validation.CheckMode;
-import org.eclipse.xtext.validation.IResourceValidator;
-import org.eclipse.xtext.validation.Issue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,7 +43,6 @@ public class EFactoryJavaValidatorTest {
 	@Inject ResourceSet rs;
 	@Inject ParseHelper<EObject> parseHelper;
 	@Inject ValidatorTester<EFactoryJavaValidator> tester;
-	@Inject IResourceValidator resourceValidator;
 	
 	@BeforeClass // This *HAS* to be @BeforeClass, a @Before doesn't work 
 	public static void setUp() throws Exception {
@@ -62,13 +54,7 @@ public class EFactoryJavaValidatorTest {
 		InputStream is = getClass().getResourceAsStream("/BuilderTests/NameAttribute.efactory");
 		assertNotNull(is);
 		EObject testModel = parseHelper.parse(is, URI.createURI("BuilderTests/NameAttribute.efactory"), null, rs);
-		AssertableDiagnostics diags = tester.validate(testModel);
-		for (Diagnostic diag : diags.getAllDiagnostics()) {
-			System.out.println(diag.toString());
-		}
-		diags.assertDiagnosticsCount(1);
-		diags.assertErrorContains("Cannot name");
-		// TODO assert it's the right error, not just any
+		tester.validate(testModel).assertErrorContains("Cannot name");
 	}
 
 	/**
@@ -81,10 +67,6 @@ public class EFactoryJavaValidatorTest {
 	@Test
 	public void testOnlyOneErrorForMissingRequiredProperty() throws Exception {
 		EObject testModel = parseHelper.parse("use testmodel.* TestModel { }");
-		List<Issue> issues = resourceValidator.validate(testModel.eResource(), CheckMode.ALL, null);
-		for (Issue issue : issues) {
-			System.out.println(issue.toString());
-		}
-		assertEquals(1, issues.size());
+		tester.validate(testModel).assertErrorContains("The required feature");
 	}
 }
