a561a222a4c95a29b558448a131c843387d3d6ee
Michael Vorburger <mike@vorburger.ch>
2014-10-04 12:34:11 +0200

    Fixed up license headers from EPL EDL dual license to only EPL http://blog2.vorburger.ch/2014/10/eclipse-public-license-enforcement-in.html http://jira.codehaus.org/browse/MLICENSE-113

--- org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/tests/util/tests/ESONWithTestmodelInjectorProviderTest.java
+++ org.eclipse.emf.eson.tests/src/org/eclipse/emf/eson/tests/util/tests/ESONWithTestmodelInjectorProviderTest.java
@@ -2,13 +2,12 @@
  * #%L
  * org.eclipse.emf.eson.tests
  * %%
- * Copyright (C) 2014 - 2014 Michael Vorburger
+ * Copyright (C) 2014 Michael Vorburger
  * %%
- * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * #L%
  */
 package org.eclipse.emf.eson.tests.util.tests;
