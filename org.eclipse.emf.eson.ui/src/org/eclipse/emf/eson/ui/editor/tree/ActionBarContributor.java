a8f61dc17ac94cb932f3f9578723fbe74ce403ac
Michael Vorburger <mike@vorburger.ch>
2014-11-05 11:08:05 +0100

    CQ: ActionBarContributor Copyright removed, as it's purely gen. code

--- org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/editor/tree/ActionBarContributor.java
+++ org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/editor/tree/ActionBarContributor.java
@@ -2,27 +2,13 @@
  * #%L
  * org.eclipse.emf.eson.ui
  * %%
- * Copyright (C) 2010 ProxiAD
+ * No copyright, as per discussion in https://dev.eclipse.org/ipzilla/show_bug.cgi?id=8690
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * #L%
- *
- * Contributors:
- *    Obeo - initial API and implementation
- */
-/**
- * Copyright (c) 2010 ProxiAD
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Obeo - initial API and implementation
  */
 package org.eclipse.emf.eson.ui.editor.tree;
 
