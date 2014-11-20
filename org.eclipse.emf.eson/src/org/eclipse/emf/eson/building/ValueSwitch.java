7e7faa130f971b6ed34f26eb299439656ac0c1cd
Michael Vorburger <mike@vorburger.ch>
2014-11-20 18:28:02 +0100

    post CQ: Minor formatting fix to two license headers

--- org.eclipse.emf.eson/src/org/eclipse/emf/eson/building/ValueSwitch.java
+++ org.eclipse.emf.eson/src/org/eclipse/emf/eson/building/ValueSwitch.java
@@ -3,7 +3,7 @@
  * org.eclipse.emf.eson
  * %%
  * Copyright (C) 2002 - 2014 Sebastian Benz, Michael Vorburger and others
- * %
+ * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- org.eclipse.emf.eson/src/org/eclipse/emf/eson/serialization/ValueSwitch.java
+++ org.eclipse.emf.eson/src/org/eclipse/emf/eson/serialization/ValueSwitch.java
@@ -2,7 +2,7 @@
  * #%L
  * org.eclipse.emf.eson
  * %%
- * Copyright (C) 2002- 2014 Sebastian Benz, Michael Vorburger and others
+ * Copyright (C) 2002 - 2014 Sebastian Benz, Michael Vorburger and others
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
